9c6800a8e602cdde8b3b8d1d0d2fb62a91e12f96
sourav349 <kumarsourav2401@gmail.com>
2024-09-22 02:23:11 +0530

    new commit

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
-import myContext from "../context/data/myContext";
+import myContext from "../../context/data/myContext";
+
 function Testimonial() {
   const context = useContext(myContext);
   const { mode } = context;
   return (
     <div>
-      <section className="text-gray-600 body-font mb-10">
-        <div className="container px-5 py-10 mx-auto">
+      <section className="">
+        <div className=" container mx-auto px-5 py-10">
           <h1
             className=" text-center text-3xl font-bold text-black"
             style={{ color: mode === "dark" ? "white" : "" }}
@@ -42,7 +43,7 @@ function Testimonial() {
                   style={{ color: mode === "dark" ? "#ff4162" : "" }}
                   className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase"
                 >
-                  SOURAV KUMAR AGARWAL
+                  Kamal Nayan Upadhyay
                 </h2>
                 <p
                   style={{ color: mode === "dark" ? "white" : "" }}
@@ -52,6 +53,7 @@ function Testimonial() {
                 </p>
               </div>
             </div>
+
             <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
               <div className="h-full text-center">
                 <img
